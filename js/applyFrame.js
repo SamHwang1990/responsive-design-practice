8b0f0f33c6ce60d08b28bc42f4ca6de17dd953b3
SamHwang1990 <samhwang1990@gmail.com>
2014-08-10 20:15:43 +0800

    add tag toggle js

--- js/applyFrame.js
+++ js/applyFrame.js
@@ -2,3 +2,27 @@
  * Created by Sam on 14-8-6.
  * ApplyFrame JS
  */
+
+$(document).ready(function(){
+    var tab = $("#tab-body .tab");
+    tab.click(function(e){
+        e.preventDefault();
+        var dataType = $(this).attr("data-type");
+        var dataState = $(this).attr("data-state");
+        if(dataState == "active")
+            return ;
+        else{
+            $(this).siblings().attr("data-state","inactive");
+            $(this).attr("data-state","active");
+            var tabBodyClass = dataType + "-body";
+            $(".tab-body").each(function(index){
+                if($(this).hasClass(tabBodyClass)){
+                    $(this).attr("data-state-body","active");
+                }else{
+                    $(this).attr("data-state-body","inactive");
+                }
+            })
+
+        }
+    })
+})
